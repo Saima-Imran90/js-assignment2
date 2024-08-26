c0956497604522844ca376a20741f016733c14f5
Saima Imran <smalldeeds1234@gmail.com>
2024-08-26 21:35:42 +0500

    Add files via upload

---
+++ java assigement2/script.js
@@ -0,0 +1,24 @@
+// Declaring variable in one statement//
+
+var apple=12,oranges=6,banana=10;
+
+
+// Declare 5 legal & 5 illegal variable names//
+
+LEGAL5
+Saima_Imran
+ContactNumber
+Apple$20
+oranges
+
+
+illegal5
+
+var
+let
+alert
+document
+int
+
+
+
