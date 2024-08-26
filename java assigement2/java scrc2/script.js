93508d7bd51440b4669c52aceeadb6946268d2ef
Saima Imran <smalldeeds1234@gmail.com>
2024-08-27 01:47:47 +0500

    Add files via upload

---
+++ java assigement2/java scrc2/script.js
@@ -0,0 +1,125 @@
+//Chapter 5 Question #1//
+document.write("<h1>Question #1</h1>")
+
+document.write("The sum of 3+5 is 8.</br>")
+
+document.write("The subtraction of 9-1 is 8.</br> ")
+
+document.write("The multiplication of 5*7 is 35.</br>")
+
+document.write("The division of 36 and 6 is 6.</br>")
+
+document.write("The %age of 450/500 is 90%.</br>")
+
+
+// Question #2//
+document.write("<h1>Question #2</h1>")
+  document.write("Value of variable=?? </br>")
+  document.write("Initial value=5,increment it</br>")
+  document.write("Value after increment is:6, add 7 in it</br>")
+  document.write("Value after addition is:13,decrement from it</br>")
+  document.write("Value after decrement is:12,divide it by 3</br>")
+  document.write("The reminder is:4")
+  
+  document.write("<h1>Question #3</h1>")
+document.write("var ticket price is=600PKR </br> ")
+document.write("var ticket is=5 </br>")
+document.write("600*5=3000 </br>")
+document.write("Cost to buy 5 tickets to a movie is 3000PKR </br>")
+
+document.write("<h1>Question #4</h1>")
+
+document.write("<h2>Table of 9</h2>")
+document.write("9*1=9 </br>")
+document.write("9*2=18 </br>")
+document.write("9*3=27 </br>")
+document.write("9*4=36 </br>")
+document.write("9*5=45 </br>")
+document.write("9*6=54 </br>")
+document.write("9*7=63 </br>")
+document.write("9*8=72 </br>")
+document.write("9*9=81 </br>")
+document.write("9*10=90")
+
+document.write("<h1>Question #6</h1>")
+document.write("C=25 </br>")
+document.write("F=(C*9/5)+32 </br>")
+document.write("F=(25*9/5)+32 </br>")
+document.write("F=77 </br>")
+document.write("Now, C=(F-32)*5/9 </br>")
+document.write("C=(77-32)*5/9 </br>")
+document.write("C=25")
+
+document.write("<h1>Question #7</h1>")
+document.write("<h2>Shopping Cart</h2>")
+document.write("Price of item 1=500</br>")
+document.write("Price of item 2=500</br>")
+document.write("Ordered quantity of item 1=8</br>")
+document.write("Ordered quantity of item 2=6</br>")
+document.write("Shipping charges=100</br>")
+document.write("Total cost of ur order is 7100")
+
+document.write("<h1>Question #8</h1>")
+document.write("<h2>Marksheet</h2>")
+document.write("Total Marks=980</br>")
+document.write("Marks Obtained=804</br>")
+document.write("Percentage=82.04%</br>")
+
+document.write("<h1>Question #9</h1>")
+document.write("<h2>Currency in PKR</h2>")
+document.write("10 US Dollars</br>")
+document.write("25 Saudi Riyals</br>")
+document.write("Exchange rates of 1 Us dollar in PKR=104.80</br>")
+document.write("Exchange rates of 1 Saudi Riyal in PKR=28</br>")
+document.write("10 US Dollars are 1048 in PKR</br>")
+document.write("25 Saudi Riyals are 700 in PKR</br>")
+document.write("Total Currency in PKR is 1748")
+
+document.write("<h1>Question #10</h1>")
+document.write("Initial number=3</br>")
+document.write("Add 5 in initial number:3+5=8</br>")
+document.write("then multiply 8 by 10=80</br>")
+document.write("Divide 80 by 2:80/2=40</br>")
+
+document.write("<h1>Question #11</h1>")
+document.write("<h2>Age Calculator</h2>")
+document.write("Current year:2024</br>")
+document.write("Birth year:2013</br>")
+document.write("your age will be either 10 0r 11 years old")
+
+document.write("<h1>Question #12</h1>")
+document.write("<h2>The Geometrizer</h2>")
+document.write("Radius of a circle:20</br>")
+document.write("The circumference is 125.67999</br>")
+document.write("The area is 1256.8</br>")
+
+document.write("<h1>Question #13</h1>")
+document.write("<h2>The Lifetime Supply Calculator</h2>")
+document.write("Favourite Snack:Nuggets</br>")
+document.write("Current Age:30</br>")
+document.write("Estimated Maximum age:70</br>")
+document.write("Amoount of snacks per day:3</br>")
+document.write("You will need 120 nuggets to last you until ripe old age of 70years")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
